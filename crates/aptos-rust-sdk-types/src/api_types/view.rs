6584a2fa36327c1116a017e1e6245202c41f3d0a
Spielcrypto <spieljs@gmail.com>
2025-07-09 03:20:53 +0500

    fix payload

--- crates/aptos-rust-sdk-types/src/api_types/view.rs
+++ crates/aptos-rust-sdk-types/src/api_types/view.rs
@@ -5,6 +5,6 @@ use crate::api_types::type_tag::TypeTag;
 #[derive(Clone, Debug, Eq, Hash, PartialEq, Serialize, Deserialize)]
 pub struct ViewRequest {
     pub function: String,
-    pub ty_args: Vec<TypeTag>,
-    pub args: Vec<Vec<u8>>,
+    pub type_arguments: Vec<TypeTag>,
+    pub arguments: Vec<Vec<u8>>,
 }
